5c449679e63cef80925b41c538597cf474c7c2cb
Ryan Richard <klaethecoder+Gen1@gmail.com>
2022-04-15 11:08:22 -0600

    Added and got working the Vistor Counter in the Footer of the page.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-const visited = fetch("https://900kkpx3k1.execute-api.us-east-1.amazonaws.com/Deploy/readvisitors")
-                .then(obj => obj['tester'])
-console.log(visited)
+fetch("https://900kkpx3k1.execute-api.us-east-1.amazonaws.com/Deploy/readvisitors")
+                .then(obj => obj.json())
+                .then(data => document.getElementById('counter').innerText = data.body.tester['N']);
+
